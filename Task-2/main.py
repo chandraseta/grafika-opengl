b4eb84d023510e87b4b9dfe02123ea5170128084
rionaldichandraseta <rionaldi.chandraseta@gmail.com>
2018-03-27 23:55:17 +0700

    Added animation for all image layers

--- Task-2/main.py
+++ Task-2/main.py
@@ -8,11 +8,12 @@ from triangle import draw_triangle
 
 if __name__ == '__main__':
     images = []
-    images.append(parse("data/images/regalia_flat.svg"))
-    images.append(parse("data/images/regalia_flat.svg"))
+
+    # Back to Front
     images.append(parse("data/images/clouds.svg"))
     images.append(parse("data/images/mountain.svg"))
     images.append(parse("data/images/building_back.svg"))
     images.append(parse("data/images/building_front.svg"))
     images.append(parse("data/images/road.svg"))
+    images.append(parse("data/images/regalia_flat.svg"))
     draw(images)
