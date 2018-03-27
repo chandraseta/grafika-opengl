01a1c97395511fddb596f18ea5e5eea982ba4eeb
rionaldichandraseta <rionaldi.chandraseta@gmail.com>
2018-03-28 00:16:22 +0700

    Added music

--- Task-2/main.py
+++ Task-2/main.py
@@ -6,7 +6,14 @@ from svgparser import *
 from car import *
 from triangle import draw_triangle
 
+from pygame import mixer
+
 if __name__ == '__main__':
+
+    pygame.mixer.init()
+    pygame.mixer.music.load("data/sounds/stand-by-me.mp3")
+    pygame.mixer.music.play(-1, 0.0)
+
     images = []
 
     # Back to Front
