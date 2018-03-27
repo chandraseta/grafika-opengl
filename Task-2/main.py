112ad58b19b9407e85f2acee7838ec9523245c4a
Vincent Hendryanto Halim <13515089@std.stei.itb.ac.id>
2018-03-27 22:15:28 +0700

    Added background

--- Task-2/main.py
+++ Task-2/main.py
@@ -10,4 +10,9 @@ if __name__ == '__main__':
     images = []
     images.append(parse("data/images/regalia_flat.svg"))
     images.append(parse("data/images/regalia_flat.svg"))
+    images.append(parse("data/images/clouds.svg"))
+    images.append(parse("data/images/mountain.svg"))
+    images.append(parse("data/images/building_back.svg"))
+    images.append(parse("data/images/building_front.svg"))
+    images.append(parse("data/images/road.svg"))
     draw(images)
