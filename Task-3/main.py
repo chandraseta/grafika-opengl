638090859d833daa60c6ed0cce2aad8dbaf5065a
rionaldichandraseta <rionaldi.chandraseta@gmail.com>
2018-04-06 21:35:12 +0700

    Removed unnecessary files from task 3

--- Task-3/main.py
+++ Task-3/main.py
@@ -2,18 +2,12 @@
 import sys
 sys.path.append('src/')
 
-from car import *
 from car3D import startShowcase
 from objparser import parseModel
-from svgparser import parseImage
 
 from pygame import mixer
 
 if __name__ == '__main__':
-
-    # pygame.mixer.init()
-    # pygame.mixer.music.load("data/sounds/car-customization.mp3")
-    # pygame.mixer.music.play(-1, 0.0)
 
     colors = ["#333333"]
 
