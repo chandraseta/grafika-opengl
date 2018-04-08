30279069a6a1f227a9a4cf198572c2cbe53d3dc6
Vincent Hendryanto Halim <13515089@std.stei.itb.ac.id>
2018-04-09 00:37:33 +0700

    Added parser

--- Task-4/src/objparser.py
+++ Task-4/src/objparser.py
@@ -1,66 +1,55 @@
 import numpy
 import Model
 
-def parseModel(filename, isColored, colors):
-    vertices = []
-    v_parts = []
-    indices = []
-    f_parts = []
+def parseModel(filename):
+    model = []
+    mesh_cnt = 0
 
     file = open(filename,"r")
     lines = file.readlines()
-    clrID = 0
-    v_current = -1
-    f_current = -1
-    v_set_count = 0
-    f_set_count = -1
+
+    index = []
+    vertex = []
+    normal = []
+    texture = []
+    finalizing = False
     for line in lines:
         line_split = line.rstrip("\n").split(" ")
-
         if (len(line_split) > 0):
-            if (line_split[0] == 'o'):
-                v_set_count += 1
-                f_set_count += 1
-
-        if (len(line_split) == 4):
-            if (line_split[0] == 'v'):
-                if (v_current != v_set_count):
-                    v_current += 1
-                    if (len(v_parts) > 0):
-                        vertices.append(v_parts)
-                    v_parts = []
-                    v_parts.append(convertColor(colors[clrID]))
-                    if isColored:
-                        clr += 1
-                v_split_temp = []
-                for id, val in enumerate(line_split):
-                    if (id > 0):
-                        v_split_temp.append(float(val)/200)
-                v_parts.append(v_split_temp)
+            if (line_split[0] == 'o'):       
+                # New Mesh
+                mesh_cnt += 1
+            elif (line_split[0] == 'v'):
+                vertex.append([float(line_split[1])/200,float(line_split[2])/200,float(line_split[3])/200])
+            elif (line_split[0] == 'vn'):
+                normal.append([float(line_split[1]),float(line_split[2]),float(line_split[3])])
+            elif (line_split[0] == 'vt'):
+                texture.append([float(line_split[1]),float(line_split[2])])
             elif (line_split[0] == 'f'):
-                if (f_current != f_set_count):
-                    f_current += 1
-                    if (len(f_parts) > 0):
-                        indices.append(f_parts)
-                    f_parts = []
-                f_split_temp = []
-                for id, val in enumerate(line_split):
-                    if (id > 0):
-                        index = line_split[id].split("/")
-                        f_split_temp.append(int(index[0]) - 1)
-                f_parts.append(f_split_temp)
+                finalizing = True
+                index.append([line_split[1],line_split[2],line_split[3]])
+            elif(finalizing):
+                buffer = processArray(index,vertex,normal,texture)    
+                index = []
+                vertex = []
+                normal = []
+                texture = []
+                finalizing = False
 
-    vertices.append(v_parts)
-    indices.append(f_parts)
+                #mesh = Mesh() # Load List of values ke 1 mesh
+                #model.append(mesh) # Masukin mesh ke model
 
-    return vertices, indices
+    #return model
 
-def convertColor(, color):
-    hexcolor = []
-    hexcolor.append(int(color[1:3],16)/255)
-    hexcolor.append(int(color[3:5],16)/255)
-    hexcolor.append(int(color[5:7],16)/255)
-    return hexcolor
+def processArray(index, vertex, normal, texture):
+    buffer = []
+    for faces in index:
+        for point in faces:
+            indexes = point.split("/")
+            buffer += vertex[int(indexes[0])-1]
+            buffer += texture[int(indexes[1])-1]
+            buffer += normal[int(indexes[2])-1]
+    return buffer
 
 if __name__ == '__main__':
-    image = parse("../data/models/regalia.obj")
+    parseModel("../data/models/box.obj")
