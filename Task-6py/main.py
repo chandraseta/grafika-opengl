2b384420cfdd0c6687d4e5cb44cd6db2abd9a5a6
Vincent Hendryanto Halim <13515089@std.stei.itb.ac.id>
2018-05-15 18:55:18 +0700

    Changed to glut

--- Task-6py/main.py
+++ Task-6py/main.py
@@ -8,50 +8,14 @@ from pygame.locals import *
 from pygame.constants import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from OpenGL.GLUT import *
 
 # IMPORT OBJECT LOADER
 from objloader import *
 from particle import *
 
-pygame.init()
-viewport = (800,600)
-hx = viewport[0]/2
-hy = viewport[1]/2
-srf = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF)
-
-glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0))
-glLightfv(GL_LIGHT0, GL_AMBIENT, (0.2, 0.2, 0.2, 1.0))
-glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0))
-glEnable(GL_LIGHT0)
-glEnable(GL_LIGHTING)
-glEnable(GL_COLOR_MATERIAL)
-glEnable(GL_DEPTH_TEST)
-glShadeModel(GL_SMOOTH)           # most obj files expect to be smooth-shaded
-
-# LOAD OBJECT AFTER PYGAME INIT
-# obj = OBJ(sys.argv[1], swapyz=True)
-
-clock = pygame.time.Clock()
-
-glMatrixMode(GL_PROJECTION)
-glLoadIdentity()
-width, height = viewport
-gluPerspective(90.0, width/float(height), 1, 100.0)
-glEnable(GL_DEPTH_TEST)
-glMatrixMode(GL_MODELVIEW)
-
-rx, ry = (0,0)
-tx, ty = (0,0)
-zpos = 5
-rotate = move = False
-
-# Load params from file
-smoke_params = json.load(open('config/smoke_config.json'))
-smoke = ParticleSystem(0,0,smoke_params)
-
-while 1:
-    for e in pygame.event.get():
-        if e.type == QUIT:
+def display():
+    if e.type == QUIT:
             sys.exit()
         elif e.type == KEYDOWN and e.key == K_ESCAPE:
             sys.exit()
@@ -74,12 +38,58 @@ while 1:
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
-
     # RENDER OBJECT
-    # glTranslate(tx/20., ty/20., - zpos)
-    # glRotate(ry, 1, 0, 0)
-    # glRotate(rx, 0, 1, 0)
-    # glCallList(obj.gl_list)
-    smoke.update()
+    glTranslate(tx/20., ty/20., - zpos)
+    glRotate(ry, 1, 0, 0)
+    glRotate(rx, 0, 1, 0)
+    glCallList(obj.gl_list)
 
-    pygame.display.flip()
+    smoke.update()
+    glutSwapBuffers()
+
+def mousePress:
+
+
+viewport = (800,600)
+glutInit(sys.argv)
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+glutInitWindowSize(800,600)
+glutCreateWindow("hello")
+glutDisplayFunc(display)
+glutMouseFunc(mousePress)
+glClearColor(0,0,0,1.0)
+hx = viewport[0]/2
+hy = viewport[1]/2
+
+glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0))
+glLightfv(GL_LIGHT0, GL_AMBIENT, (0.9, 0.9, 0.9, 1.0))
+glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0))
+glEnable(GL_LIGHT0)
+glEnable(GL_LIGHTING)
+glEnable(GL_COLOR_MATERIAL)
+glEnable(GL_DEPTH_TEST)
+glShadeModel(GL_SMOOTH)           
+
+# most obj files expect to be smooth-shaded
+# LOAD OBJECT AFTER PYGAME INIT
+obj = OBJ(sys.argv[1], swapyz=True)
+
+clock = pygame.time.Clock()
+
+glMatrixMode(GL_PROJECTION)
+glLoadIdentity()
+width, height = viewport
+gluPerspective(90.0, width/float(height), 1, 100.0)
+glEnable(GL_DEPTH_TEST)
+glMatrixMode(GL_MODELVIEW)
+
+rx, ry = (0,0)
+tx, ty = (0,0)
+zpos = 5
+rotate = move = False
+
+# Load params from file
+smoke_params = json.load(open('config/smoke_config.json'))
+smoke = ParticleSystem(0,0,smoke_params)
+
+glutMainLoop()
